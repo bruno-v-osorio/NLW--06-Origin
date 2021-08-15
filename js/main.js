99eb335a6308bf9c5567f6570cfea4218d2ccffb
Bruno Vargas <53790911+bruno-v-osorio@users.noreply.github.com>
2021-08-15 15:43:13 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/* TOGGLE MENU */
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function(){
+        nav.classList.toggle('show')
+    })
+}
+
+/* FECHAR MENU AO CLICAR NOS LINKS */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+    })
+}
+
+/* HEADER - ADD BOX SHADOW QUANDO SCROLLAR */
+
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll',function() {
+    if(window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+    } else {
+    header.classList.remove('scroll')
+    }
+})
+
